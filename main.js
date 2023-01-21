940ea33c7fc51cdfcf5a0f43a5bd9f464c9a9f1c
diana guarin <dianamar8723@hotmail.com>
2023-01-21 16:10:51 -0500

    no hay commit

--- main.js
+++ main.js
@@ -1,41 +1,62 @@
- 
- const botonFavorito = document.getElementsByTagName("button")
- console.log(botonFavorito);
- 
- 
+
+const botonFavorito = document.getElementsByClassName("cardsElements")
 
 
+function addProduct (){
+forEach(item => {
+
+});
+
+
+}
 
 function mostrarPoductos(arrayProductos) {
 
-    const cards = document.getElementById("cards");
+  const cards = document.getElementById("cards");
 
-    for (let iterator of arrayProductos) {
-        
-        let cardsElements = document.createElement("div")
-        cardsElements.className = "cardsElements";
-        cardsElements.innerHTML = `<img src=${iterator.image}>
+  for (let iterator of arrayProductos) {
+
+    let cardsElements = document.createElement("div")
+    cardsElements.className = "cardsElements";
+    cardsElements.innerHTML = `<img src=${iterator.image}>
                                     <h1>${iterator.title}</h1>                            
                                     <p>${iterator.description}</p>
                                     <h2>${iterator.price}</h2>
-                                    <button> favoritos </button>`
+                                    <button class="botonFavorito">favoritos</button>`
 
-        cards.appendChild(cardsElements)
-    }
+    cards.appendChild(cardsElements)
+  }
+  const botonFavorito = document.getElementsByClassName("botonFavorito");
+  //console.log(botonFavorito);
 
-  localStorage.setItem("productos", arrayProductos)
+  for (let i = 0; i < botonFavorito.length; i++) {
+   // console.log(botonFavorito[i]);
+   
+  
+  }
 }
+
+
 
 async function fetchData() {
 
-    const respons = await fetch('https://fakestoreapi.com/products')
-    const data = await respons.json();
+  const respons = await fetch('https://fakestoreapi.com/products')
+  const data = await respons.json();
 
-    mostrarPoductos(data);
+  mostrarPoductos(data);
 
 }
 
 fetchData()
+
+Swal.fire({
+  icon: 'info',
+  title: 'agregado favoritos',
+  text: 'se agrego correctamente!',
+
+})
+
+
 
 
 
